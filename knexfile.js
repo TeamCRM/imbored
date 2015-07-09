f49e58b21491454a3d2c13671bce780906ce4d3a
Zillabot <michael@howyoulove.com>
2015-07-09 11:10:17 -0700

    no big change

--- knexfile.js
+++ knexfile.js
@@ -13,10 +13,10 @@ module.exports = {
   pool: {
     min: 2,
     max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
+  }//,
+  // migrations: {
+  //   tableName: 'knex_migrations'
+  // }
 
 };
 
