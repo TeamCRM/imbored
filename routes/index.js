9722fde9591a5f47ddd8ce554f95aabf29371645
[Chauncey Gard] <[ncgard621@gmail.com]>
2015-07-09 11:02:09 -0700

    Adding backbone underscore jquery

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 
+app.use(express.static('public'));
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('regis', { title: "I'm Bored!" });
+});
+
+router.get('/js', function(req, res, next) {
+	res.serve('../js');
 });
 
 module.exports = router;
