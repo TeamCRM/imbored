ca5809d907ea7a7140665d1718e7046f8440af35
Zillabot <michael@howyoulove.com>
2015-07-14 13:50:49 -0700

    hash and salt stored in DB

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var app = require('../app');
-// var pwd = require('pwd');
+var pwd = require('pwd');
 var knexConfig = require('../knexfile.js')
 var knex = require('knex')(knexConfig);
 var database = app.get('database');
@@ -11,8 +11,16 @@ var database = app.get('database');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 app.use(express.static('public'));
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  
+  // if (req.cookies.email) {
+  //   username = req.cookies.email;
+  // } else {
+  //   username = null;
+  // };  
+    
 
   res.render('index', { title: "I'm Bored!" });
 });
@@ -29,7 +37,7 @@ router.get('/register', function (req,res,next){
 router.post('/register', function (req,res){
 	
 	// console.log(req.body)
-	console.log(req.body[1])
+	// console.log(req.body[1])
 	// Selects all of the usernames stored in the user name column that match the requested username
 	knex('authtable').where('username', req.body.email)
 		.then(function(result){
@@ -37,33 +45,44 @@ router.post('/register', function (req,res){
 
 			if(result.length===0){
 				var prefArr= [];
-				console.log(req.body[17])
+				// console.log(req.body[17])
 				for(var i=0;i<20;i++){
 					var k= parseInt(i)
-					console.log(req.body[k])
+					// console.log(req.body[k])
 					if(req.body[k]){
 					prefArr.push(k)
-					console.log(prefArr)
+					// console.log(prefArr)
 					}
 				}
-				console.log(req.body)
-				console.log(prefArr)
-				knex('authtable').insert([{username:req.body.email}])
-				.then(function() {
-					knex('authtable').where('username',req.body.email).select('userid')
-						.then(function(results){
-							console.log(results)
-						
-					for(var j=0;j<prefArr.length;j++){
-					knex('useridtable').insert([{preferenceid:prefArr[j],userid:results[0].userid }])
-						.then(function(){
-				
-				console.log('worked!!!')
-				res.redirect('/')
-							})
-						}
-					})
+  
+      pwd.hash(req.body.password, function(err,salt,hash){
+        var stored = {salt:salt, hash:hash};
+        console.log(stored);
+        
+        knex('authtable').insert(stored)
+          .then(function() {
+            response.cookie('username', email)
+  
+          })
+      })
+        
+			// console.log(req.body)
+			// console.log(prefArr)
+			knex('authtable').insert([{username:req.body.email}])
+			.then(function() {
+				knex('authtable').where('username',req.body.email).select('userid')
+					.then(function(results){
+						// console.log(results)      
+            
+				for(var j=0;j<prefArr.length;j++){
+				knex('useridtable').insert([{preferenceid:prefArr[j],userid:results[0].userid }])
+					.then(function(){      
+			// console.log('worked!!!')
+			res.redirect('/results')
+						})
+					}
 				})
+			})
 	// 		}else{
 	// knex('authtable').insert([{username:req.body.email/* hash:stored.hash,salt: stored.salt,userid: req.body.id*/}])
 	// 		.then(function(){ 
