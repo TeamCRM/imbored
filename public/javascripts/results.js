5104afc6d985666d5d900d25333096dcf80c62d4
Richard Johnson <rjohnson2590@gmail.com>
2015-07-17 16:04:24 -0700

    Making more views

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -18,11 +18,7 @@ $(document).ready(function() {
 	// var newValue= '{"'+value[0]+'"'+value+'}'
 	// var goodValue=JSON.parse(newValue)
 	console.log(goodValue[0].length)
-	// var arr= ["cafe",'gym','park']
-	for (var j=0;j<goodValue[0].length;j++){
-	$.getJSON('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+goodValue[0][j]+'+in+Portland&key=AIzaSyA6GqWRLxW7Lxvzunccd_Gg5VtMOVR6Zb4', function(data) {
-		// console.log(data.results)	
-		var ResultsModel = Backbone.Model.extend({
+			var ResultsModel = Backbone.Model.extend({
 	    	defaults :{'name':'','id':'', 'phone':'','website':'','price':'','rating':''},
 	    	details : function (id) {
 				var model = this
@@ -88,13 +84,18 @@ $(document).ready(function() {
 				this.$el.html('<span>'+arr+'</span>')
 			}
 		})
+	// var arr= ["cafe",'gym','park']
+	for (var j=0;j<goodValue[0].length;j++){
+	$.getJSON('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+goodValue[0][j]+'+in+Portland&key=AIzaSyA6GqWRLxW7Lxvzunccd_Gg5VtMOVR6Zb4', function(data) {
+		// console.log(data.results)	
+
 		var results= new ResultsModel({})
 		var resultsCollection = new ResultsCollection([results]);
 		var collectionView= new ResultsCollectionView({collection:resultsCollection, model:results })
 		// var otherArr =["cafe",'gym','park']
 		var value = getCookie('preferences');
-	var newValue= value.split(':')
-	var goodValue=JSON.parse("[" + newValue[1] + "]");
+		var newValue= value.split(':')
+		var goodValue=JSON.parse("[" + newValue[1] + "]");
 		for(var h=0;h<1;h++){
 			var str=''
 			for(var k=0;k<goodValue[0].length;k++){
