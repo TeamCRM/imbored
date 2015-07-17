517254ef5b632f3f4d7518ea827511c19adfc8d6
Richard Johnson <rjohnson2590@gmail.com>
2015-07-17 15:45:16 -0700

    Have results displayed based on preference cookie

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -1,19 +1,40 @@
 // hasChanged for backbone
 $(document).ready(function() {
-	$.getJSON('https://maps.googleapis.com/maps/api/place/textsearch/json?query=cafe+in+Portland&key=AIzaSyCLGyZUzAn-ATeMtqZyW_BwYSIuv9KY7mY', function(data) {
-		console.log(data.results)
+	console.log(document.cookie)
+ function getCookie(name)
+  {
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    return (value != null) ? unescape(value[1]) : null;
+  }
+//  function getCookie(name) {
+//   var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
+//   var result = regexp.exec(document.cookie);
+//   return (result === null) ? null : result[1];
+// }
+	var value = getCookie('preferences');
+	var newValue= value.split(':')
+	var goodValue=JSON.parse("[" + newValue[1] + "]");
+	// var newValue= '{"'+value[0]+'"'+value+'}'
+	// var goodValue=JSON.parse(newValue)
+	console.log(goodValue[0].length)
+	// var arr= ["cafe",'gym','park']
+	for (var j=0;j<goodValue[0].length;j++){
+	$.getJSON('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+goodValue[0][j]+'+in+Portland&key=AIzaSyA6GqWRLxW7Lxvzunccd_Gg5VtMOVR6Zb4', function(data) {
+		// console.log(data.results)	
 		var ResultsModel = Backbone.Model.extend({
 	    	defaults :{'name':'','id':'', 'phone':'','website':'','price':'','rating':''},
 	    	details : function (id) {
 				var model = this
 				console.log(id)
-				$.getJSON('https://maps.googleapis.com/maps/api/place/details/json?placeid='+id+'&key=AIzaSyCLGyZUzAn-ATeMtqZyW_BwYSIuv9KY7mY', function (details){
+				$.getJSON('https://maps.googleapis.com/maps/api/place/details/json?placeid='+id+'&key=AIzaSyA6GqWRLxW7Lxvzunccd_Gg5VtMOVR6Zb4', function (details){
 					console.log(details.result)
 					model.set({'phone':details.result.formatted_phone_number,'website': details.result.website,'price': details.result.price_level,'rating': details.result.rating});
 				console.log('endJSON')
 				}) 
 			}
 		});
+
 		var ResultsView=Backbone.View.extend({
 			render: function(){
 				var name = this.model.get('name');
@@ -61,20 +82,40 @@ $(document).ready(function() {
 				this.fetch();
 			}
 		});
+		var ResultsCollectionView= Backbone.View.extend({
+			render: function(arr){
+				this.$el=$('#encompass')
+				this.$el.html('<span>'+arr+'</span>')
+			}
+		})
 		var results= new ResultsModel({})
 		var resultsCollection = new ResultsCollection([results]);
+		var collectionView= new ResultsCollectionView({collection:resultsCollection, model:results })
+		// var otherArr =["cafe",'gym','park']
+		var value = getCookie('preferences');
+	var newValue= value.split(':')
+	var goodValue=JSON.parse("[" + newValue[1] + "]");
+		for(var h=0;h<1;h++){
+			var str=''
+			for(var k=0;k<goodValue[0].length;k++){
+				str+=goodValue[0][k]+'  '
+			}
+			collectionView.render(str)
+		}
 		for(var i=0; i<data.results.length;i++){
 			var results= new ResultsModel({});
 			results.set({'name': data.results[i].name, 'id': data.results[i].place_id})
-			
 			var view = new ResultsView({collection:resultsCollection, model:results })
 			var detailedView=new ResultsMiniView({
 				model:results	
 			});
-			view.render()
+			view.render();
+			
 			$('#prefResults').append(view.$el);	
+			// $('#encompass').append(collectionView.$el)
 		}	
 	});
+	}
 });
 
 
