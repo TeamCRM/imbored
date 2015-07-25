b4dda7e203fe2a6e969d922f6e8e9c5045af0251
Zillabot <michael@howyoulove.com>
2015-07-24 17:19:23 -0700

    regis.jade prefs list into table

--- public/javascripts/imBoredServe.js
+++ public/javascripts/imBoredServe.js
@@ -1,8 +1,9 @@
-
+//Preference List
 var prefArr =["amusement_park","art_gallery","aquarium","bowling_alley","movie_theater","campground","cafe","restaurant","library","museum","park","shopping_mall","stadium","zoo","university","swimming","spa","hiking","book_store","night_club"]
 
+//Register Page: Preference Table
 function apinameToFriendlyName(apiname){
-	return apiname+"!"
+	return apiname;
 }
 var friendlyPrefs= prefArr.map(apinameToFriendlyName)
 	
@@ -18,8 +19,8 @@ var prefGrid= function(prefArr, colCount){
 	return happyArr;
 }
 
-var happyArr = prefGrid(friendlyPrefs, 4);
-var renderColumn = function (col) {
+var happyArr = prefGrid(friendlyPrefs, 5);
+var renderColumn = function(col) {
 	return "<tr>" + col.map(renderRow)+ "</tr> \n";
 
 }
@@ -28,10 +29,11 @@ var renderRow = function(friendlyPref, i) {
 	return "\t <td>" + '<input type="checkbox" name="'+ i +'" >'+ friendlyPref + "</td> \n";
 }
 
+$(".prefs").append(happyArr.map(renderColumn).join("\n"));
+
 console.log(happyArr.map(renderColumn).join("\n"));
 
-
-
+//Check Password 
 var checkPassword = function() {
 	console.log('Whatever');
     if($('#password').val().length > 0 && $('#password').val() === $('#password_confirm').val()) {
