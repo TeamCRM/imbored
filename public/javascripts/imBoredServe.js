40ca4c2368f3efd963c4456867930e6be10b44b3
Zillabot <michael@howyoulove.com>
2015-07-27 16:51:51 -0700

    ImBoredServe built table to display preferences, Added bootstrap and modified stye.css

--- public/javascripts/imBoredServe.js
+++ public/javascripts/imBoredServe.js
@@ -2,38 +2,54 @@
 var prefArr =["amusement_park","art_gallery","aquarium","bowling_alley","movie_theater","campground","cafe","restaurant","library","museum","park","shopping_mall","stadium","zoo","university","swimming","spa","hiking","book_store","night_club"]
 
 //Register Page: Preference Table
-function apinameToFriendlyName(apiname){
-	return apiname;
-}
-var friendlyPrefs= prefArr.map(apinameToFriendlyName)
+
+// function apinameToFriendlyName(apiname){
+// 	return apiname;
+// }
+// var friendlyPrefs= prefArr.map(apinameToFriendlyName)
+// 	
+// 
+// var prefGrid= function(prefArr, colCount){
+// 	var rowCount = Math.ceil(prefArr.length / colCount);
+// 	var happyArr = [];
+// 	for(var i = 0; i<prefArr.length; i+= rowCount) {
+// 		var splitGrid= prefArr.slice(i, rowCount+i);
+// 		happyArr.push(splitGrid);
+// 	}
+// 	return happyArr;
+// }
+// 
+// var happyArr = prefGrid(friendlyPrefs, 5);
+// var renderColumn = function(col) {
+// 	return "<tr class='prefRow'>" + col.map(renderRow)+ "</tr>";
+// }
+// 
+// var renderRow = function(friendlyPref, i) {
+// 	return "<td>" + '<input type="checkbox" name="'+ i +'" >'+ friendlyPref.replace('_', ' ') + "</td>";
+// }
+
+var height = 5
+var width = 4
+var html = ""
+var counter= 0
+for(var i= 0; i< height; i++) {
+	html+= '<tr>'
+
+	for(var j= 0; j< width; j++) {
+		var mult = j+counter;
+		console.log(mult)
+		html += '<td ><label><input type=checkbox name= '+ mult +'>'+ prefArr[mult].replace('_', ' ') + '</label></td>'
 	
-
-var prefGrid= function(prefArr, colCount){
-	var rowCount = Math.ceil(prefArr.length / colCount);
-	var happyArr = [];
-	for(var i = 0; i<prefArr.length; i+= rowCount) {
-		var splitGrid= prefArr.slice(i, rowCount+i);
-		happyArr.push(splitGrid);
 	}
-
-	return happyArr;
+	html += '</tr>'
+	counter += 4
 }
+console.log(html)
+$(".prefs").append(html);
 
-var happyArr = prefGrid(friendlyPrefs, 5);
-var renderColumn = function(col) {
-	return "<tr>" + col.map(renderRow)+ "</tr> \n";
+// console.log(happyArr.map(renderColumn).join("\n"));
 
-}
-
-var renderRow = function(friendlyPref, i) {
-	return "\t <td>" + '<input type="checkbox" name="'+ i +'" >'+ friendlyPref + "</td> \n";
-}
-
-$(".prefs").append(happyArr.map(renderColumn).join("\n"));
-
-console.log(happyArr.map(renderColumn).join("\n"));
-
-//Check Password 
+//Login Check Password 
 var checkPassword = function() {
 	console.log('Whatever');
     if($('#password').val().length > 0 && $('#password').val() === $('#password_confirm').val()) {
