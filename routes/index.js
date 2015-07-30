75877f954807da0f192e1d4055d9aa41f5f83e0c
Zillabot <michael@howyoulove.com>
2015-07-30 15:26:35 -0700

    syntax error corrected

--- routes/index.js
+++ routes/index.js
@@ -1,195 +1,196 @@
-  var express = require('express');
-  var bodyParser = require('body-parser');
-  var router = express.Router();
-  var app = require('../app');
-  var pwd = require('pwd');
-  var knexConfig = require('../knexfile.js');
-  var knex = require('knex')(knexConfig);
-  var database = app.get('database');
-  var request = require('request');
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: false
-  }));
-  app.use(express.static('public'));
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var app = require('../app');
+var pwd = require('pwd');
+var knexConfig = require('../knexfile.js');
+var knex = require('knex')(knexConfig);
+var database = app.get('database');
+var request = require('request');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: false
+}));
+app.use(express.static('public'));
 
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
+/* GET home page. */
+router.get('/', function(req, res, next) {
 
-    if (req.cookies.preferences) {
-      res.render('results', {
-        title: "I'm Bored!"
-      });
-    } else {
-      res.render('login', {
-        title: "I'm Bored!"
-      });
-    };
-  });
-
-  //Login 
-  router.post('/', function(req, res) {
-    model=this
-    knex('authtable')
-    .where({
-      'username': req.body.username
-    })
-    .then(function(records) {
-
-      var user = records[0];
-
-      if (records.length === 0) {
-        res.render('login', {
-          title: 'Im Bored',
-          user: null,
-          error: 'No Such User'
-        });
-
-      } else {
-        pwd.hash(req.body.password, user.salt, function(err, hash) {
-
-          if (err) {
-            console.log(err);
-          }
-
-          if (user.hash === hash) {
-
-              //Get user ID from DB
-              knex('authtable')
-              .where({
-                'username': req.body.username
-              }).select('userid')
-              .then(function(results) {
-
-                var userid = results[0].userid
-
-                  //Get user preferences from DB 
-                  knex('userpreftable')
-                  .where({
-                    'userid': results[0].userid
-                  }).select('preferenceid')
-                  .then(function(result) {
-
-                    var prefs = [];
-
-                      //Store user preferences in prefs array
-                      for (var prop in result) {
-                        if (prop !== 'userid') {
-                          prefs.push(result[prop].preferenceid)
-                        }
-                      }
-
-                      var prefName = [];
-
-                      for (var i = 0; i < prefs.length; i++) {
-                        if (i !== prefs.length - 1) {
-
-                          knex('preftable').where({
-                            'preferenceid': prefs[i]
-                          }).select('apiname')
-                          .then(function(rezult) {
-                            prefName.push(rezult[0].apiname);
-                          });
-
-                        } else if (i == prefs.length - 1) {
-
-                          knex('preftable').where({
-                            'preferenceid': prefs[i]
-                          }).select('apiname')
-                          .then(function(rezult) {
-                            prefName.push(rezult[0].apiname);
-                            
-
-                            request('https://maps.googleapis.com/maps/api/geocode/json?address='+req.body.city+'&key=AIzaSyD0OGfjwg9iGIWxr-IUCVHCFI8EWPl-HbI', function(err, resp,body){
-                              console.log(typeof body)
-                              var citySelect= JSON.parse(body)
-                              console.log(citySelect.results)
-                              if(citySelect.results.length !== 0){
-                                res.cookie('preferences', prefName.join());
-                                res.cookie('lat',citySelect.results[0].geometry.location.lat )
-                                res.cookie('lng',citySelect.results[0].geometry.location.lng )
-                                res.redirect('/results');
-                              } else{
-                                res.render('login', {
-                                  title: 'Im Bored',
-                                  user: null,
-                                  error: 'Not a valid location. Please try again'
-                                });
-                              }
-                            })
-
+if (req.cookies.preferences) {
+res.render('results', {
+title: "I'm Bored!"
 });
-}
-}
-});
-});
-
 } else {
+res.render('login', {
+title: "I'm Bored!"
+});
+};
+});
+
+//Login 
+router.post('/', function(req, res) {
+
+knex('authtable')
+.where({
+'username': req.body.username
+})
+.then(function(records) {
+
+var user = records[0];
+
+if (records.length === 0) {
   res.render('login', {
     title: 'Im Bored',
     user: null,
-    error: 'Incorrect Password '
+    error: 'No Such User'
+  });
+
+} else {
+  pwd.hash(req.body.password, user.salt, function(err, hash) {
+
+    if (err) {
+      console.log(err);
+    }
+
+    if (user.hash === hash) {
+
+      //Get user ID from DB
+      knex('authtable')
+        .where({
+          'username': req.body.username
+        }).select('userid')
+        .then(function(results) {
+
+          var userid = results[0].userid
+
+          //Get user preferences from DB 
+          knex('userpreftable')
+            .where({
+              'userid': results[0].userid
+            }).select('preferenceid')
+            .then(function(result) {
+
+              var prefs = [];
+
+              //Store user preferences in prefs array
+              for (var prop in result) {
+                if (prop !== 'userid') {
+                  prefs.push(result[prop].preferenceid)
+                }
+              }
+
+              var prefName = [];
+
+              for (var i = 0; i < prefs.length; i++) {
+                if (i !== prefs.length - 1) {
+
+                  knex('preftable').where({
+                      'preferenceid': prefs[i]
+                    }).select('apiname')
+                    .then(function(rezult) {
+                      prefName.push(rezult[0].apiname);
+                    });
+
+                } else if (i == prefs.length - 1) {
+
+                  knex('preftable').where({
+                      'preferenceid': prefs[i]
+                    }).select('apiname')
+                    .then(function(rezult) {
+                      
+                      prefName.push(rezult[0].apiname);
+                      request('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.body.city + '&key=AIzaSyD0OGfjwg9iGIWxr-IUCVHCFI8EWPl-HbI', function(err, resp, body) {
+                      
+                        var citySelect = JSON.parse(body)
+                
+                        if (citySelect.results.length !== 0) {
+                          
+                          res.cookie('preferences', prefName.join());
+                          res.cookie('lat', citySelect.results[0].geometry.location.lat)
+                          res.cookie('lng', citySelect.results[0].geometry.location.lng)
+                          res.redirect('/results');
+                          
+                        } else {
+                          res.render('login', {
+                            title: 'Im Bored',
+                            user: null,
+                            error: 'Not a valid location. Please try again'
+                          });
+                        }
+                      })
+                    });
+                }
+              }
+            });
+        });
+
+    } else {
+      res.render('login', {
+        title: 'Im Bored',
+        user: null,
+        error: 'Incorrect Password '
+      });
+    }
   });
 }
 });
-}
-});
 });
 
-  //Render Results Page
-  router.get('/results', function(req, res, next) {
+//Render Results Page
+router.get('/results', function(req, res, next) {
 
-    res.render('results', {
-      title: "I'm Bored!"
-    });
-  });
-
-
+res.render('results', {
+title: "I'm Bored!"
+});
+});
 //Logout and clear all cookies 
 router.get('/logout', function(req, res, next) {
 
-  for (var it in $.cookie()) $.removeCookie(it);
-  
-  res.render('logout', {
+res.clearCookie('preferences');
+res.clearCookie('lat');
+res.clearCookie('lng');
+
+res.render('logout', {
+  title: "I'm Bored!"
+});
+
+//Render Register Page
+router.get('/register', function(req, res, next) {
+
+  res.render('regis', {
     title: "I'm Bored!"
-  });
+  })
+});
 
-  //Render Register Page
-  router.get('/register', function(req, res, next) {
+//Register 
+router.post('/register', function(req, res) {
 
-    res.render('regis', {
-      title: "I'm Bored!"
-    })
-  });
+  var prefArr = [];
+  var prefs = [];
+  var prefName = [];
 
-  //Register 
-  router.post('/register', function(req, res) {
-
-    var prefArr = [];
-    var prefs = [];
-    var prefName = [];
-
-    knex('authtable')
+  knex('authtable')
     .where({
       'username': req.body.username
     })
     .then(function(result) {
 
-        //Hash and salt   
-        pwd.hash(req.body.password, function(err, salt, hash) {
+      //Hash and salt   
+      pwd.hash(req.body.password, function(err, salt, hash) {
 
-          var stored = {
-            username: req.body.username,
-            salt: salt,
-            hash: hash
-          };
+        var stored = {
+          username: req.body.username,
+          salt: salt,
+          hash: hash
+        };
 
-          knex('authtable')
+        knex('authtable')
           .returning('userid')
           .insert(stored)
           .then(function(userid) {
-            console.log(req.body)
+      
             for (var prop in req.body) {
+              
               if (prop !== 'username' && prop !== 'city' && prop !== 'password' && prop !== 'password_confirm') {
                 prefs.push(prop);
               }
@@ -199,62 +200,68 @@ router.get('/logout', function(req, res, next) {
 
               knex('preftable').where({
                 'preferenceid': prefs[i]
-              }).select('apiname')
-              .then(function(rezult) {
-                console.log(rezult)
-                prefName.push(rezult[0].apiname);
-                    // res.cookie('preferences', prefName.join());
-                  });
+                }).select('apiname')
+                .then(function(rezult) {
+                  
+                  prefName.push(rezult[0].apiname);
+                });
             }
 
             knex('authtable')
-            .where({
-              'username': req.body.username
-            }).select('userid')
-            .then(function(results) {
+              .where({
+                'username': req.body.username
+              }).select('userid')
+              .then(function(results) {
 
-              if (results.length !== 0) {
+                if (results.length !== 0) {
 
-                for (var i = 0; i < 20; i++) {
-                  var k = parseInt(i)
-                  if (req.body[k]) {
-                    prefArr.push(k)
+                  for (var i = 0; i < 20; i++) {
+                    
+                    var k = parseInt(i)
+                    
+                    if (req.body[k]) {
+                      prefArr.push(k)
+                    }
                   }
-                }
 
-                for (var j = 0; j < prefArr.length; j++) {
+                  for (var j = 0; j < prefArr.length; j++) {
+                    
+                    knex('userpreftable')
+                      .insert([{
+                        preferenceid: prefArr[j],
+                        userid: results[0].userid
+                      }])
+                      .then()
+                  }
+
                   knex('userpreftable')
-                  .insert([{
-                    preferenceid: prefArr[j],
-                    userid: results[0].userid
-                  }])
-                  .then()
-                }
-
-                knex('userpreftable')
-                .then(function() {
-                 request('https://maps.googleapis.com/maps/api/geocode/json?address='+req.body.city+'&key=AIzaSyD0OGfjwg9iGIWxr-IUCVHCFI8EWPl-HbI', function(err, resp,body){
-                  console.log(typeof body)
-                  var citySelect= JSON.parse(body)
-                  if(citySelect.results.length !== 0){
-                    res.cookie('preferences', prefName.join());
-                    res.cookie('lat',citySelect.results[0].geometry.location.lat )
-                    res.cookie('lng',citySelect.results[0].geometry.location.lng )
-                    res.redirect('/results');
-                  } else{
-                    res.render('regis', {
-                      title: 'Im Bored',
-                      user: null,
-                      error: 'Not a valid location. Please try again'
+                    .then(function() {
+                      
+                      request('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.body.city + '&key=AIzaSyD0OGfjwg9iGIWxr-IUCVHCFI8EWPl-HbI', function(err, resp, body) {
+                    
+                        var citySelect = JSON.parse(body)
+                        
+                        if (citySelect.results.length !== 0) { 
+                          
+                          res.cookie('preferences', prefName.join());
+                          res.cookie('lat', citySelect.results[0].geometry.location.lat)
+                          res.cookie('lng', citySelect.results[0].geometry.location.lng)
+                          res.redirect('/results');
+                          
+                        } else {
+                          res.render('regis', {
+                            title: 'Im Bored',
+                            user: null,
+                            error: 'Not a valid location. Please try again'
+                          });
+                        }
+                      })
                     });
-                  }
-                })
-               });
-              }
-            });
-});
-});
-});
+                }
+              });
+          });
+      });
+    });
 });
 
-  module.exports = router;
+module.exports = router;
